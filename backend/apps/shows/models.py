d5772c85c72a9442d8690c4bb57fc9c3b8e6f294
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-12 22:46:38 +0600

    Update

---
+++ backend/apps/shows/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from multiselectfield import MultiSelectField
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=255)
+    photo = models.ImageField(blank=True, null=True, upload_to='actors/photos/')
+
+    def __str__(self):
+        return self.full_name
+
+
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    rating = models.PositiveIntegerField()
+    actors = models.ManyToManyField(Actor)
+
+    type_choices = (('film', 'Film'),
+                    ('tv', 'TV Show'))
+    type = models.CharField(max_length=255, choices=type_choices)
+    preview = models.ImageField(blank=True, null=True, upload_to='shows/previews/')
+    video = models.FileField(upload_to='shows/films/', blank=True, null=True)
+
+    # TODO: Remove defaults
+    description = models.TextField(default="")
+
+    genre_choices = (('action', 'action'),
+                     ('comedy', 'comedy'),
+                     ('drama', 'drama'),
+                     ('fantasy', 'fantasy'),
+                     ('horror', 'horror'),
+                     ('mystery', 'mystery'),
+                     ('romance', 'romance'),
+                     ('thriller', 'thriller'),
+                     ('western', 'western'))
+    genre = MultiSelectField(choices=genre_choices, default="")
+
+    def __str__(self):
+        return f"{self.type.upper()}: «{self.title}»"
+
+    class Meta:
+        verbose_name = "Шоу"
+        verbose_name_plural = "Шоу"
---
+++ backend/core/settings.py
@@ -0,0 +1,132 @@
+import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+SECRET_KEY = '-b=v&rqnem*1i&@ibi)on461m@taw1&h30p4fd-5lrus4f)kc%'
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '*'
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third party
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'multiselectfield',
+    'drf_yasg',
+    'corsheaders',
+
+    # own
+    'accounts',
+    'shows'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'accounts.User'
+CORS_ORIGIN_ALLOW_ALL = True
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user': 'accounts.serializers.UserSerializer',
+        'user_create': 'accounts.serializers.UserSerializer',
+        'current_user': 'accounts.serializers.UserSerializer',
+    },
+    'HIDE_USERS': False,
+    'PERMISSIONS': {
+        'user': ['rest_framework.permissions.IsAuthenticated']
+    }
+}
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'lvltawit3@gmail.com'
+# EMAIL_HOST_PASSWORD = 'parolnetlvl3'
+#
+# DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
