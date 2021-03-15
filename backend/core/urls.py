18d6e32e4f325858383dcf1e3a8cb20b609aa395
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-15 09:28:10 +0600

    Remove redoc

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('api/v1/', include('shows.urls')),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
